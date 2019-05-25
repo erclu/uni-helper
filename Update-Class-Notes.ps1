1dd9ada9359113fbacda440c811f6a3b31ea2443
erclu <30255227+erclu@users.noreply.github.com>
2019-05-25 21:32:41 +0200

    move to new pc, add mypy and update packages

--- Update-Class-Notes.ps1
+++ Update-Class-Notes.ps1
@@ -1,4 +1,4 @@
-cd D:\_Programming\uni-helper
+cd $PSScriptRoot
 pipenv run python notes/organizer.py
 Write-Host -NoNewline "Press any key to close..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
